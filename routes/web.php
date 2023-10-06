1b9d3bc5984326597a0b0251c87f0ece7d3efa75
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-10-06 11:11:17 +0600

    3.3 done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('v1')->group( function (){
+    Route::resource('/tasks', \App\Http\Controllers\Api\V1\TaskController::class);
+});
