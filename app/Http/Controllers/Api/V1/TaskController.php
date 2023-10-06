b865945247b879e60fbfa45d51c8236794a0e6c2
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-10-06 12:11:58 +0600

    3.5 done

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
+use Illuminate\Http\Request;
+
 
 class TaskController extends Controller
 {
@@ -18,7 +20,9 @@ class TaskController extends Controller
 
     public function store(StoreTaskRequest $request)
     {
-        //
+        $task = Task::create($request->validated());
+
+        return TaskResource::make($task);
     }
 
 
